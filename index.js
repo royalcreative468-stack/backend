ab25b9f7ad12a0f0f82c868811b25fc6ada2e3e1
unknown <royalcreative468@gmail.com>
2025-09-28 17:41:59 +0530

    cors origin url updated

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions = {
-    origin:'https://frontend-liart-six-41.vercel.app/',
+    origin:'https://frontend-liart-six-41.vercel.app',
     credentials:true
 }
 
